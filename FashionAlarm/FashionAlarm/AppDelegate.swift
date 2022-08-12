3aa59be41377678f4bf0762ea8d5974a9d224dc8
SEOKJUN KO <tjrwns1021@gmail.com>
2022-08-12 14:53:12 +0900

    Start new project
    i will make new project about alarm
    this app will make alarm with recommendation about clothes related to weahther

---
+++ FashionAlarm/FashionAlarm/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  FashionAlarm
+//
+//  Created by 고석준 on 2022/08/12.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ FashionAlarm/FashionAlarm/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FashionAlarm
+//
+//  Created by 고석준 on 2022/08/12.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
