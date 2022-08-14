210c4dc887625048e86c7d663ec4557b3fd8bdc6
SEOKJUN KO <tjrwns1021@gmail.com>
2022-08-14 23:54:59 +0900

    Relocate func to extension
    literally, i relocate the func to extension[ SetLocationViewContoller ]

--- FashionAlarm/FashionAlarm/SetLocationViewController.swift
+++ FashionAlarm/FashionAlarm/SetLocationViewController.swift
@@ -27,6 +27,27 @@ class SetLocationViewController: UIViewController {
         loadCoordinates()
     }
     
+    @IBAction func forwardGeoCoding(_ sender: Any) {
+        guard let address = locationTextField.text else { return }
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) { placemarks, error in
+            guard let placemark = placemarks?.first else {
+                self.detailResult.text = "지역 검색 결과가 없습니다."
+                self.detailResult.isHidden = false
+                return
+            }
+            self.detailResult.isHidden = false
+            let address = "\(placemark.locality ?? "") \(placemark.name ?? "")"
+            self.detailResult.text = address
+            guard let lat = placemark.location?.coordinate.latitude else { return }
+            guard let lon = placemark.location?.coordinate.longitude else { return }
+            self.saveCoordinates(latitude: lat, longitude: lon, address: address)
+            self.mapView.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: lon), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.001)), animated: false)
+        }
+    }
+}
+
+extension SetLocationViewController {
     private func saveCoordinates(latitude: Double, longitude: Double, address: String){
         let data = [ "latitude": latitude, "longitude": longitude ]
         let userDefaults = UserDefaults.standard
@@ -45,25 +66,6 @@ class SetLocationViewController: UIViewController {
         detailResult.isHidden = false
         self.detailResult.text = address
         self.mapView.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: lon), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.001)), animated: false)
-    }
-    
-    @IBAction func forwardGeoCoding(_ sender: Any) {
-        guard let address = locationTextField.text else { return }
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(address) { placemarks, error in
-            guard let placemark = placemarks?.first else {
-                self.detailResult.text = "지역 검색 결과가 없습니다."
-                self.detailResult.isHidden = false
-                return
-            }
-            self.detailResult.isHidden = false
-            let address = "\(placemark.locality ?? "") \(placemark.name ?? "")"
-            self.detailResult.text = address
-            guard let lat = placemark.location?.coordinate.latitude else { return }
-            guard let lon = placemark.location?.coordinate.longitude else { return }
-            self.saveCoordinates(latitude: lat, longitude: lon, address: address)
-            self.mapView.setRegion(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: lon), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.001)), animated: false)
-        }
     }
 }
 
