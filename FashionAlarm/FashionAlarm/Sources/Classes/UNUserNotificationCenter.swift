683f12882095fbbcab7bf1c4497d0a563147ccf1
SEOKJUN KO <tjrwns1021@gmail.com>
2022-08-26 15:06:09 +0900

    Change sound file
    i found rule about sound file.
    the rule is that sound file can't over 30 sec.
    so i change sound file

--- FashionAlarm/FashionAlarm/Sources/Classes/UNUserNotificationCenter.swift
+++ FashionAlarm/FashionAlarm/Sources/Classes/UNUserNotificationCenter.swift
@@ -13,7 +13,7 @@ extension UNUserNotificationCenter {
         content.title = "일어날 시간이에요!"
         content.body = "앱을 실행시켜 오늘의 날씨에 맞는 옷 카테고리를 확인하세요"
 //        content.sound = .default
-        content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "Morning-Kiss.caf") )
+        content.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "Morning-Kiss.mp3") )
         
         let component = Calendar.current.dateComponents([.hour, .minute], from: alert.date) // 테이블 셀에 해당하는 alert 객체에서 시, 분을 뽑아 컴퍼넌트 만듬
         let trigger = UNCalendarNotificationTrigger(dateMatching: component, repeats: alert.isOn) // 특정 시간에 알림 발송하는데 사용 // repeats = 반복여부
