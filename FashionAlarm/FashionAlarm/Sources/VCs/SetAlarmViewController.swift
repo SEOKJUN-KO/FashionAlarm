86821253d5dcf8f8cc207c154aec75f8bb91ec42
SEOKJUN KO <tjrwns1021@gmail.com>
2022-09-07 20:25:43 +0900

    Remove useless code in SetAlarmViewController
    same as title

--- FashionAlarm/FashionAlarm/Sources/VCs/SetAlarmViewController.swift
+++ FashionAlarm/FashionAlarm/Sources/VCs/SetAlarmViewController.swift
@@ -22,7 +22,6 @@ class SetAlarmViewController: UIViewController {
     
     // 데이트 피커 타이머 초기화가 1분이기 떄문에 60초로
     var duration = 60
-    var timerStatus: TimerStatus = .end // 타이머의 초기값
     var currentSeconds = 0 // 현재 카운트 다운되고 있는 시간을 초로 저장하는 프로퍼티
     var timer: DispatchSourceTimer? // 타이머
     var audioPlayer: AVAudioPlayer?
@@ -41,42 +40,28 @@ class SetAlarmViewController: UIViewController {
         self.datePicker.addTarget(self, action: #selector(dateChange(datePikcer:)), for: UIControl.Event.valueChanged)
         self.offMusicBtn.isHidden = true
     }
-    
+    // 취소 버튼 클릭 시
     @IBAction func tapCancelButton(_ sender: UIButton) {
-        switch self.timerStatus {
-        case .start:
-            self.stopTimer()
-            self.timerStatus = .end
-        default:
-            break
-        }
+        self.stopTimer()
     }
     
-    // 시작, 재개 버튼 클릭 시
+    // 시작 버튼 클릭 시
     @IBAction func tapStartButton(_ sender: UIButton) {
+        // UI 초기 설정
         // 타이머에 해당하는 시간을 초단위로 변환
-        
-        switch self.timerStatus {
-        case .end:
-            // 타이머 시작 시
-            self.timerStatus = .start
-            self.selectedTime = self.datePicker.date
-            if ( Int(self.selectedTime.timeIntervalSinceNow - Date().timeIntervalSinceNow) <= 0 ){
-                self.selectedTime = calendar.date(byAdding: .hour, value: 24, to: self.datePicker.date) ?? Date()
-            }
-            UIView.animate(withDuration: 0.5, animations: {
-                self.timerLabel.alpha = 1
-                self.progressView.alpha = 1
-                self.datePicker.alpha = 0
-            })
-            self.startButton.isHidden = true
-            self.cancelButton.isEnabled = true
-            self.startTimer()
-            //
-        case .start:
-            // 타이머 시작모드에서 일기정지버튼 누를 시
-            self.startButton.isHidden = true
+        self.selectedTime = self.datePicker.date
+        if ( Int(self.selectedTime.timeIntervalSinceNow - Date().timeIntervalSinceNow) <= 0 ){
+            self.selectedTime = calendar.date(byAdding: .hour, value: 24, to: self.datePicker.date) ?? Date()
         }
+        UIView.animate(withDuration: 0.5, animations: {
+            self.timerLabel.alpha = 1
+            self.progressView.alpha = 1
+            self.datePicker.alpha = 0
+        })
+        self.startButton.isHidden = true
+        self.cancelButton.isEnabled = true
+        // 알람이 울릴 때 까지 동작해야 할 코드
+        self.startTimer()
     }
     
     @IBAction func stopMusic(_ sender: Any) {
@@ -157,7 +142,6 @@ extension SetAlarmViewController {
         // UI 작업
         self.offMusicBtn.isHidden = true
         self.iterSwitch.isHidden = false
-        self.timerStatus = .end
         self.cancelButton.isHidden = true
         self.startButton.isHidden = false
         self.setLocationBtn.isHidden = false
